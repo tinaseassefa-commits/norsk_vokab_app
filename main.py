e5eb479aeebf5af873d91ff4895733fba82c105d
tinaseassefa-commits <tinaseassefa@gmail.com>
2026-05-02 20:26:01 +0200

    small fix

--- main.py
+++ main.py
@@ -54,4 +54,6 @@ async def get_word():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
